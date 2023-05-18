9d20498bf1bb11a5a8fba1015200bbb6def19613
Nicolas Donada <nicolasdonada12@gmail.com>
2023-05-17 22:14:19 -0300

    atualizaoção

--- test.py
+++
@@ -1,9 +0,0 @@
-def nome_usuario(nome):
-    mensagem = "Olá " + nome + ", Bem Vindo!"
-    return mensagem
-
-
-while n == True:
-    n = input("Digite seu nome: ")
-    r = nome_usuario(n)
-    print(r)
--- test2.py
+++
@@ -1,7 +0,0 @@
-def soma(a, b):
-    s = a + b
-    print(s)
-
-soma(2,6)
-soma(9,54)
-soma(3214,5438)
